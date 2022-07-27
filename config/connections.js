70c1ed22a6bf775ed21196fe141f44f08465e5f1
mohamedraficp1 <mohamedraficp@gmail.com>
2022-07-27 14:37:53 +0530

    atlas link changed

--- config/connections.js
+++ config/connections.js
@@ -1,7 +1,21 @@
-const mongoose= require('mongoose')
-mongoose.connect("mongodb://localhost/royalsuites", ()=>{
-    console.log("Connected")
-},
-e=>{
-    console.log("Connection error")
+// require('dotenv').config()
+// const mongoose= require('mongoose')
+// mongoose.connect(process.env.MONGODB_CONNECTION_STRING, ()=>{
+//     console.log("Connected")
+// },
+// e=>{
+//     console.log("Connection error")
+// })
+
+require('dotenv').config()
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING,{
+
+    useNewUrlParser:true
+
+}).then((data)=>{
+    console.log('mongodb atlas connected');
+    return data
+}).catch((e)=>{
+    console.log(e+"mongodb atlas connection failed");
 })
